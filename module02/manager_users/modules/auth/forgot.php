969c5606186895e61ad609a4dfb5b35d202be70f
tien.nguyen9 <tien.nguyen9@ntq-solution.com.vn>
2024-01-05 16:51:45 +0700

    feat:add sql

--- module02/manager_users/modules/auth/forgot.php
+++ module02/manager_users/modules/auth/forgot.php
@@ -65,7 +65,8 @@ $msg_type = getFlashData('msg_type');
 
 <div class="row">
     <div class='col-4' style="margin: 50px auto;">
-        <h2 class="text-center text-uppercase">Quên mật khẩu</h2>
+        <h3 class="text-center text-uppercase">Nhập địa chỉ email để lấy lại mật khẩu</h3>
+        <br />
         <?php
         if (!empty($msg)) {
             getMsg($msg, $msg_type);
