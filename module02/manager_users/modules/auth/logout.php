262f5f845476989e46b59d85236618339edbb1c9
tien-huce-edu <tien195164@huce.edu.vn>
2024-01-06 13:01:24 +0700

    feat: done authentication and authorization

--- module02/manager_users/modules/auth/logout.php
+++ module02/manager_users/modules/auth/logout.php
@@ -1,4 +1,14 @@
-<!-- dang xuat --><?php
+<!-- dang xuat -->
+<?php
 if (!defined('_CODE')) {
     die("Error! access not allow");
 }
+
+
+if (isLogin()) {
+    $token = getSession('loginToken');
+    delete('tokenlogin', "token='$token'");
+    removeSession('loginToken');
+    redirect('?module=auth&action=login');
+}
+?>
